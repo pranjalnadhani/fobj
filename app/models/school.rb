658a7a30fc27644e8b8995e6cb0dc82782fbc23e
Pranjal <pranjalnadhani@gmail.com>
2013-11-29 14:22:10 +0530

    Add schools.

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,9 @@
 class School < ActiveRecord::Base
+  validates :name_of_school, presence: true
+  validates :principal_name, presence: true
+  validates :type, presence: true
+  validates :place, presence: true
+  validates :affiliated_to, presence: true
+  validates :email, presence: true
+  validates :phone_no, presence: true
 end
